1b3f427e9728d705cf33cce65382e7c6952255d6
Robby Roda <95758113+CoderSigma@users.noreply.github.com>
2023-12-19 11:19:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,7 @@
 import requests as pla
 import argparse,os
+from common.spec import kill
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--url', help='url example is https://site.com/', required=True)
-args = parser.parse_args()
 
-file = 'Header.json'
-r = pla.get(args.url)
-tt = str(r.headers)
-if os.path.exists('Header.json'):
-    os.remove('Header.json')
-    print('Server :',r.headers['Server'])
-    if not r.headers['Server'] == r.headers['Server']:
-        pass
-    else:
-        print('Server :',r.headers['Server'])
-    if not r.headers.get('X-XSS-Protection') == r.headers.get('X-XSS-Protection'):
-        pass
-    else:
-        print('XSS-Protection :',r.headers.get('X-XSS-Protection'))
-    if not r.headers['Cache-Control'] == r.headers['Cache-Control']:
-        pass
-    else:
-        print('Cache Control :', r.headers['Cache-Control'])
-    if not r.headers.get('Set-Cookie') == r.headers.get('Set-Cookie'):
-        pass
-    else:
-        print('Cookies :', r.headers.get('Set-Cookie'))
-    if not r.headers.get('User-Agent') == r.headers.get('User-Agent'):
-       pass
-    else:
-        print('User-Agent :',r.headers.get('User-Agent'))
-   
-else:
-    with open('Header.json', 'a') as f:
-        f.write(tt)
-        f.close()
-
-with open('Header.json', 'a') as f:
-    f.write(tt.replace(" ", '||\n||')) 
-    f.close()
-
-    if not r.headers['Server'] == r.headers['Server']:
-        pass
-    else:
-        print('Server :',r.headers['Server'])
-    if not r.headers.get('X-XSS-Protection') == r.headers.get('X-XSS-Protection'):
-        pass
-    else:
-        print('XSS-Protection :',r.headers.get('X-XSS-Protection'))
-    if not r.headers['Cache-Control'] == r.headers['Cache-Control']:
-        pass
-    else:
-        print('Cache Control :', r.headers['Cache-Control'])
-    if not r.headers.get('Set-Cookie') == r.headers.get('Set-Cookie'):
-        pass
-    else:
-        print('Cookies :', r.headers.get('Set-Cookie'))
-    if not r.headers.get('User-Agent') == r.headers.get('User-Agent'):
-       pass
-    else:
-        print('User-Agent :',r.headers.get('User-Agent'))
+if __main__ == '__name__':
+    kill()
